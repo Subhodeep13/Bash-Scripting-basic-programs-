a6792c916f14efec38a72f8a32714221226121f2
Subhodeep13 <88298681+Subhodeep13@users.noreply.github.com>
2022-02-10 12:51:19 +0530

    Update My date script.sh

--- My date script.sh
+++ My date script.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-
+#specifying a function mydate which echos out date 
 mydate(){
         echo "today is:"
         date
         echo "have a GREAT day !"
 }
-
+# asking the user for an input , here as his or her name 
 echo -n "whats your name"
 read r
-
+#giving the input here with a string as salutation and further running the function 
 echo "hey $r how are you doing"
      mydate
