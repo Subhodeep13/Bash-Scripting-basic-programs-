02a61c76112cbcee3bb4f729a648c3c43624e896
Subhodeep13 <88298681+Subhodeep13@users.noreply.github.com>
2022-02-10 12:47:37 +0530

    Mydate

---
+++ My date script.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+
+mydate(){
+        echo "today is:"
+        date
+        echo "have a GREAT day !"
+}
+
+echo -n "whats your name"
+read r
+
+echo "hey $r how are you doing"
+     mydate
